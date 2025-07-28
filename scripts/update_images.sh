ed2dee0ec26e8dcaf4a9e37fbe4e9f177130a119
Qixuan Han <Qixuann@localhost>
2025-07-28 03:02:09 -0600

    modify 3 files

--- scripts/update_images.sh
+++ scripts/update_images.sh
@@ -13,7 +13,7 @@ echo Built airflow-worker:latest
 docker build . -t airflow:latest || (echo Failed to build airflow:latest && exit 1)
 echo Built airflow:latest
 
-docker tag airflow-worker:latest wanghanchengchn/airflow-worker:latest
-docker push wanghanchengchn/airflow-worker:latest
-docker tag airflow:latest wanghanchengchn/airflow:latest
-docker push wanghanchengchn/airflow:latest
+docker tag airflow-worker:latest 1481767455/airflow-worker:latest
+docker push 1481767455/airflow-worker:latest
+docker tag airflow:latest 1481767455/airflow:latest
+docker push 1481767455/airflow:latest
